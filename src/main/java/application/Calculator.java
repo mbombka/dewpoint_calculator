40fff2d15e8ed155eafe0c4e895fb8b468451b39
mbombka <mbombka@gmail.com>
2023-05-31 16:35:34 +0200

    nice looking but not working

--- src/main/java/application/Calculator.java
+++ src/main/java/application/Calculator.java
@@ -6,7 +6,7 @@ import java.util.Map;
 public class Calculator {
 
     
-    public final static Double convectionHeatTransferCoefficient = 37.4; // calculated from website
+    public final static Double convectionHeatTransferCoefficient = 37.0; // calculated from website - 370.4
     public final static Double thicknessMapResolution = 1.0; // resolution for calculation of Temperature map 
 
     //calculate dewPoint at given temperature and humidity
@@ -16,31 +16,17 @@ public class Calculator {
     }
 
 
-
-    //calculate temperature on inside(where the heat source is)  of partition, given air temperature on both sides, and thermal coefficient. 
-    public static double partitionTemperatureStep(double insideTemperature, double outsideTemperature, double thermalCoefficient, double partitionThickness) {
-        double heatConductivity = 1 / (partitionThickness * 100 / thermalCoefficient); //partition thickness is in [cm], but unit is [m] thats why *100
-        double result = insideTemperature - (heatConductivity * (insideTemperature - outsideTemperature)* theramlResistanceRsi);
-        result = Math.round(result * 100) / 100.0;
-        return result;
-    }
-
-    //calculate map of temperature for given thickness of material
-    public static Map<Double, Double> calculateTemperatureMap(int thickness, int insideTemperature, int outsideTemperature, double thermalCoefficient) {
+      //calculate map of temperature for given thickness of material
+      public static Map<Double, Double> calculateTemperatureMap(int thickness, int insideTemperature, int outsideTemperature, double thermalConductivity) {
         Map<Double, Double> temperatureMap = new HashMap<>();        
         int steps = (int) Math.round(thickness/thicknessMapResolution) + 1;
         Double[] temperatureArray = new Double[steps];
         temperatureArray[0] = (double) outsideTemperature; 
-        temperatureArray[steps - 1] = partitionTemperatureStep(insideTemperature, outsideTemperature, thermalCoefficient, thicknessMapResolution);
+        temperatureArray[steps - 1] = calculateTemperature(thickness, insideTemperature, outsideTemperature, thermalConductivity);
         
         //fill array with temporary rounded values
-        for(int i = 1; i < temperatureArray.length; i ++ ) {
+        for(int i = 1; i < temperatureArray.length ; i ++ ) {
             temperatureArray[i] = outsideTemperature +  ((double)(temperatureArray[steps - 1] - temperatureArray[0]) / steps) * i;  
-        }
-
-        //make actual calculations
-        for(int i = 1; i < steps - 1; i ++ ) {
-             temperatureArray[i] = partitionTemperatureStep(temperatureArray[i], temperatureArray[i - 1], thermalCoefficient, thicknessMapResolution);
         }
 
         //save all temperatures to map
@@ -63,9 +49,9 @@ public class Calculator {
     //T= (kTa + LhTb)/(k + Lh)
 
     public static double calculateTemperature(double thickness, int insideTemperature, int outsideTemperature, double thermalConductivity){
-        thickness = thickness / 100;////partition thickness is in [cm], but unit is [m] thats why *100
-        double T = (thermalConductivity * insideTemperature + thickness * convectionHeatTransferCoefficient * outsideTemperature) 
-                    / (thermalConductivity + thickness * convectionHeatTransferCoefficient);
+        thickness = thickness / 100.0;////partition thickness is in [cm], but unit is [m] thats why /100
+        double T = ((thermalConductivity * insideTemperature) + (thickness * convectionHeatTransferCoefficient * outsideTemperature)) 
+                    / (thermalConductivity + (thickness * convectionHeatTransferCoefficient));
        return T;
     }
 
