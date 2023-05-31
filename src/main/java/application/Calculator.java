62ea2a738cb30ccb0da47dd23ff203fb4f73961d
mbombka <mbombka@gmail.com>
2023-05-31 11:01:02 +0200

    Introduced newTemperature calculation function

--- src/main/java/application/Calculator.java
+++ src/main/java/application/Calculator.java
@@ -8,6 +8,7 @@ import java.util.Map;
 public class Calculator {
 
     public final static Double theramlResistanceRsi = 0.13 ; // Thermal resistance Rsi for horizontal heat flow (0.10 for upwards heat flow, 0.17 for downwards heat flow)
+    public final static Double convectionHeatTransferCoefficient = 307.4;
     public final static Double thicknessMapResolution = 1.0; // resolution for calculation of Temperature map 
     //outside - colder side
     //inside - hot side
@@ -18,6 +19,8 @@ public class Calculator {
         
         return  Math.round(dewpoint * 10)/10.0;
     }
+
+
 
     //calculate temperature on inside(where the heat source is)  of partition, given air temperature on both sides, and thermal coefficient. 
     public static double partitionTemperatureStep(double insideTemperature, double outsideTemperature, double thermalCoefficient, double partitionThickness) {
@@ -51,6 +54,24 @@ public class Calculator {
         }
 
         return temperatureMap;
+    }
+
+    //this calculation is based on formula Qconvection = Qconduction
+    //Qconduction = (thermal conductivity  * (Ta - T2) ) / thickness  || 
+    //Qconductivity = Convection heat transfer coefficient h * (T2 - Tb)
+    //Ta, Tb = known temperature on other sides of wall always: Ta > Tb
+    //thermal conductivity = K
+    //thickness = L
+    //Convection heat transfer coeficient h = (0.023 × Re^0.8 × Pr^0.4) × (k / D)
+    //we used 10m/s and tube diameter 160mm then, Re ≈ 1.02 × 10^6 and Pr ≈ 0.709
+    //h ≈ 307.4 W/(m^2·K)
+    //T= (kTa + LhTb)/(k + Lh)
+
+    public static double calculateTemperature(int partitionThickness, int insideTemperature, int outsideTemperature, double thermalConductivity){
+        partitionThickness = partitionThickness * 100;////partition thickness is in [cm], but unit is [m] thats why *100
+        double T = (thermalConductivity * insideTemperature + partitionThickness * convectionHeatTransferCoefficient * outsideTemperature) 
+                    / (thermalConductivity + partitionThickness * convectionHeatTransferCoefficient);
+       return T;
     }
 
 
